8b9b824f1ef67fd4df1025ee30273697e0b438b5
yangyuwei <1936786089@qq.com>
2021-04-18 10:52:38 +0800

    note

--- js/menu-data.js
+++ js/menu-data.js
@@ -151,8 +151,27 @@ np.menuData = [{
     title: '自动换行(W)',
     shortcut: '',
     enabled: true,
-    handler: null
-  }, {
+    handler: () => {
+      np.bWrap = !np.bWrap;
+      localStorage.setItem('bWrap', np.bWrap);
+
+      if(np.bWrap) {
+        $statusBar.display(false);
+        $editor.resize(false);
+        $menubar.enabled(3, 0, false);   // [查看]-[状态栏]菜单禁用
+        $menubar.enabled(1, 9, false);   // [编辑]-[转到]菜单禁用
+      } else {
+        $statusBar.display(np.bShowStatusBar);
+        $editor.resize(np.bShowStatusBar);
+        $menubar.enabled(3, 0, true);     // [查看]-[状态栏]菜单启用
+        $menubar.enabled(1, 9, true);     // [编辑]-[转到]菜单启用
+        $menubar.checked(3, 0, np.bShowStatusBar);
+      }
+
+      $menubar.checked(2, 0, np.bWrap);
+      $editor.setWrap(np.bWrap);
+    }
+  } , {
     title: '字体(F)...',
     shortcut: '',
     enabled: true,
@@ -204,6 +223,7 @@ np.menuData = [{
   width: '166px',
   left: '216px'
 }];
+
 
 
 
